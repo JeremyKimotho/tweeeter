1bebd015d76209ef652db652594365c865f39fe4
Jeremy Kimotho <jeremykimotho@yahoo.com>
2024-09-15 21:27:19 -0600

    began changes for logout button to final styling

--- tweet/static/js/base.js
+++ tweet/static/js/base.js
@@ -1,5 +1,6 @@
 const modal = new bootstrap.Modal(document.getElementById("modal"))
 const form_textareas = document.querySelectorAll('.auto-expand');
+const logout_popup = document.getElementById("logout-popover")
 
 const post_form_lite_homepage_button = document.getElementById("modal-post-lite-button");
 const post_form_lite_homepage = document.getElementById("new-post-form-lite-id");
@@ -60,5 +61,9 @@ post_form_lite_homepage.addEventListener('input', function() {
     post_form_lite_homepage_button.style.opacity="1";
     console.log("Input")
   }
+});
+
+var popover = new bootstrap.Popover(logout_popup, {
+  container: 'body'
 });
 
